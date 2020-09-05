7bff975a43946fd6e900b86757911b422b579408
ec2013 <ec2013@users.noreply.github.com>
2020-09-04 20:50:47 -0400

    use json, start search function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
+import * as questions from './questionsJson.json';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
+class  App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+		this.search = this.search.bind(this);
+	}
+	
+	search(e) {
+		var term = e.target.value;
+	}
+	
+	render() {
+		var searchResults = null;
+		
+		return (
+		<div className="App">
+			<input type="text" onChange={this.search} />
+				{searchResults}
+		</div>
   );
+	}
 }
 
 export default App;
