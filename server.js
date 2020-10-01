1743a5a48733da75c4ca597b14cf75fd38b200a7
Michael <michaelpurtill123@hotmail.com>
2020-09-30 20:23:14 -0400

    i am a god

--- server.js
+++ server.js
@@ -11,8 +11,25 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/search', (req, res) => {
-	//res.send(wtf);
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "questions"
 });
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  
+	con.query("SELECT * FROM qa where id=2", function (err, result, fields) {
+		if (err) throw err;
+		console.log(result);
+	});
+});
+
+// app.post('/api/search', (req, res) => {
+	// res.send(wtf);
+// });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
