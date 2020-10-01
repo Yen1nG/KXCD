4139b4a7ae4a5812879aa1dff32dbd26e1a82981
Michael <michaelpurtill123@hotmail.com>
2020-09-30 21:26:28 -0400

    unfuck world

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-var mysql = require('mysql');
-
-const app = express();
-const port = process.env.PORT || 5000;
-
+var express = require('express');
+var cors = require('cors');
+var app = express();
 app.use(cors());
+const bodyParser = require('body-parser');
+var mysql = require('mysql');
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,18 +16,22 @@ var con = mysql.createConnection({
   database: "questions"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+// con.connect(function(err) {
+  // if (err) throw err;
+  // console.log("Connected!");
   
-	con.query("SELECT * FROM qa where id=2", function (err, result, fields) {
-		if (err) throw err;
-		console.log(result);
-	});
-});
-
-// app.post('/api/search', (req, res) => {
-	// res.send(wtf);
+	// con.query("SELECT * FROM qa where id=2", function (err, result, fields) {
+		// if (err) throw err;
+		// console.log(result);
+	// });
 // });
+
+app.post('/api/search', (req, res) => {
+	
+	// res.header("Access-Control-Allow-Origin", "*");
+	var searchTerm = req.body.searchTerm;
+	console.log(searchTerm);
+	res.send(searchTerm);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
