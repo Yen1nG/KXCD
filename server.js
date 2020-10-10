d49a7346f9b273cd13018d624339c723b5127cee
Yening Wang <wangyening1213@hotmail.com>
2020-10-09 21:31:54 -0700

    fuck firebase

--- server.js
+++ server.js
@@ -3,26 +3,35 @@ var cors = require('cors');
 var app = express();
 app.use(cors());
 const bodyParser = require('body-parser');
-var mysql = require('mysql');
+// var mysql = require('mysql');
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "questions"
-});
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+var config = {
+	apiKey: "AIzaSyCyhgbLlPIC0WwrdE7zdHdhfN8kiJiqxqo",
+    authDomain: "kxcd-ec4bf.firebaseapp.com",
+    databaseURL: "https://kxcd-ec4bf.firebaseio.com/",
+    storageBucket: "kxcd-ec4bf.appspot.com"
+};
+firebase.initializeApp(config);
 
-con.connect(function(err) {
-		if (err) throw err;
-  
-		
-	});
+// Get a reference to the database service
+var database = firebase.database();
 
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "questions"
+// });
 
+// con.connect(function(err) {
+// 		if (err) throw err;		
+// 	});
 
 app.post('/api/search', (req, res) => {
 	
@@ -33,7 +42,6 @@ app.post('/api/search', (req, res) => {
 			if (err) throw err;
 			res.send(result);
 		});
-	
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
